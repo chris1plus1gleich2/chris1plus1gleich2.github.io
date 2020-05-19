217259099a7e92acb3117ffe71d0dc6147a9b197
chris1plus1gleich2 <59878153+chris1plus1gleich2@users.noreply.github.com>
2020-05-19 14:12:01 +0200

    Seiten hinzufügen

---
+++ action_page.php
@@ -0,0 +1,9 @@
+<?php
+
+$empfaenger = "christopher.portmann@gymansium-immensee.ch";
+$betreff = "Kontakt auf chris1plus1gleich2";
+$from = "From ";
+$text = "";
+ 
+mail($empfaenger, $betreff, $text, $from);
+?>
