f5f33f2a3ca8627491d4ea83a8dfa020504dc373
chris1plus1gleich2 <59878153+chris1plus1gleich2@users.noreply.github.com>
2020-05-19 14:39:42 +0200

    vb

--- action_page.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$empfaenger = "christopher.portmann@gymansium-immensee.ch";
-$betreff = "Kontakt auf chris1plus1gleich2";
-$from = "From ";
-$text = "";
- 
-mail($empfaenger, $betreff, $text, $from);
-?>
